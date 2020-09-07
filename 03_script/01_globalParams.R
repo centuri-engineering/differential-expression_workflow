8d538bd7fa87e482d53123f95ca6be1301907c01
’metavannier’ <thomas.vannier@univ-amu.fr>
2020-09-07 18:11:29 +0200

    1st commit of ibdm_rattier_rnaseq

---
+++ 03_script/01_globalParams.R
@@ -0,0 +1,10 @@
+###############################################################################
+# This file defines PROJECT parameters as global variables that will be loaded
+# before analysis starts.
+#
+
+
+#### Input / Output
+PATH_DATA = file.path("data")
+#PATH_OUTPUT = file.path("output");
+#PATH_PICTURE = file.path("picture")
